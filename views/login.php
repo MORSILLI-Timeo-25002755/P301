b3122c6718f21c9fd6e3078fbed68a1f28aaa0f1
MORSILLI-Timeo-25002755 <timeo.morsilli@etu.univ-amu.fr>
2026-09-21 12:12:10 +0200

    feat(login): ajout de la vue

---
+++ views/login.php
@@ -0,0 +1,13 @@
+<?php
+namespace Views; // PSR-12: head blocks must be separated by a single blank line
+class Login { // PSR-12: opening brace next line
+    public function show(): void { // PSR-12: opening brace next line
+        ?>
+        <form action="">
+            <input type="email" name="" id="">
+            <input type="password" name="" id="">
+            <input type="submit" value="">
+        </form>
+<?php
+    }
+}
