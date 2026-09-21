e86b56145f36e44a3cae354aa76a9e175951d5b5
MORSILLI-Timeo-25002755 <timeo.morsilli@etu.univ-amu.fr>
2026-09-21 11:33:38 +0200

    feat(index.php): router finished
    # Conflicts:
    #	index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require '_assets/includes/autoloader.php';
 
 $routes = [
     '/'      => \Controllers\Homepage::class,
-    '/login' => \Controllers\LoginController::class,
+    '/login' => \Controllers\Login::class,
 ];
 
 $path = rtrim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/') ?: '/';
