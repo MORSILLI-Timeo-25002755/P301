40f617efee36670fa67fd569d1d5aedd2aa61c61
Matteo Yanni <matteo.yanni@etu.univ-amu.fr>
2026-09-24 15:39:32 +0200

    feat(login): version de test terminée (sans BD)

--- controllers/login.php
+++ controllers/login.php
@@ -1,10 +1,30 @@
 <?php
+
 namespace Controllers;
+
+use models\loginModel;
+
+require_once("models/login.php");
+require_once("views/login.php");
 
 class Login
 {
     public function execute(): void
     {
-        (new \Views\Login)->show();
+        (new \Views\login())->show();
+    }
+    public function login(): void {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $email = $_POST['email'];
+            $pwd = $_POST['pwd'];
+
+            $model = new loginModel();
+            if ($model->checkLogin($email, $pwd)) {
+                (new \Controllers\Homepage())->execute();
+            } else {
+                echo "ID incorrect";
+                $this->execute();
+            }
+        }
     }
 }
