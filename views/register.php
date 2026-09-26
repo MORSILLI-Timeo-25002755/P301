fc59d474747bb27941eed5b8f594f0717e9100fe
MORSILLI-Timeo-25002755 <timeo.morsilli@etu.univ-amu.fr>
2026-09-26 15:58:10 +0200

    feat(vue-register): ajout du style

--- views/register.php
+++ views/register.php
@@ -2,39 +2,41 @@
 
 namespace Views;
 
-class Register {
-    public function show(bool $notFilled, bool $validPassword): void {
+class Register
+{
+    public function show(bool $notFilled, bool $validPassword): void
+    {
+        begin_page('Register', '_assets/css/register.css');
         ?>
-        <main>
-            <form method="POST" action="/register">
-                <fieldset>
+        <form method="POST" action="/register">
+            <fieldset>
+                <legend>Inscription</legend>
 
-                    <legend>Inscription</legend>
+                <p>Créez votre compte</p>
 
-                    <label for="idemail">E-mail :</label>
-                    <input type="email" id="idemail" name="email" autocomplete="off"/><br>
+                <label for="idemail">E-mail</label>
+                <input type="email" id="idemail" name="email" autocomplete="off" required>
 
-                    <label for="idusrn">Nom d'utilisateur :</label>
-                    <input type="text" id="idusrn" name="username" autocomplete="off"/><br>
+                <label for="idusrn">Nom d'utilisateur</label>
+                <input type="text" id="idusrn" name="username" autocomplete="off" required>
 
-                    <label for="idpwd">Mot de passe :</label>
-                    <input type="password" id="idpwd" name="pwd" autocomplete="off"/><br>
+                <label for="idpwd">Mot de passe</label>
+                <input type="password" id="idpwd" name="pwd" autocomplete="off" required>
 
-                    <label for="idconf">Confirmez votre mot de passe :</label>
-                    <input type="password" id="idconf" name="conf" autocomplete="off"/><br>
+                <label for="idconf">Confirmez votre mot de passe</label>
+                <input type="password" id="idconf" name="conf" autocomplete="off" required>
 
-                    <?php if($notFilled): ?>
-                        <p>Veuillez remplir tous les champs !</p>
-                    <?php endif; ?>
-                    <?php if(!$validPassword): ?>
-                        <p>Echec de la confirmation du mot de passe</p>
-                    <?php endif; ?>
+                <?php if ($notFilled): ?>
+                    <p class="error">Veuillez remplir tous les champs !</p>
+                <?php endif; ?>
 
-                    <input type="submit" name="send" value="Valider"/>
+                <?php if (!$validPassword): ?>
+                    <p class="error">Échec de la confirmation du mot de passe.</p>
+                <?php endif; ?>
 
-                </fieldset>
-            </form>
-        </main>
+                <input type="submit" name="send" value="Valider">
+            </fieldset>
+        </form>
         <?php
     }
 }
